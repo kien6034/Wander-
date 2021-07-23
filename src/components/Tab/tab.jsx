0b21a6ed4c8930d392a85adc4fa15b4a5d0d912d
unknown <trinhtrungkientb99@gmail.com>
2021-07-23 10:01:09 +0700

    first push

---
+++ src/components/Tab/tab.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import tabStyle from './tab.style';
+
+const CTab = (props) =>
+{
+  const {
+    cType,
+    ...rest
+  } = props;
+
+  const classes = tabStyle(rest);
+
+  return (
+    <h1 className={classes.root}>
+      {cType}
+    </h1>
+  );
+};
+
+CTab.propTypes = {
+  cType: PropTypes.string,
+};
+
+CTab.defaultProps = {
+  cType: 'New',
+};
+
+export default CTab;
---
+++ src/container/content_header/content_header.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import contentHeaderStyle from './content_header.style';
+import CustomTypography from '../../components/Typography/typography';
+import CustomInput from '../../components/Input/Input';
+
+const ContentHeader = (props) =>
+{
+  const { bgImage } = props;
+
+  const classes = contentHeaderStyle({ bgImage });
+
+  return (
+    <div className={classes.root}>
+      <div className={classes['home--jumbo']}>
+        <h1 className={classes.big}>
+          <div className={classes['heading--top']}>
+            <CustomTypography fontSize="3.5rem" color="#EF6F6C">
+              Buy &amp; Sell
+            </CustomTypography>
+          </div>
+          <div className={classes['heading--bot']}>
+            <CustomTypography fontSize="3.5rem" color="white">
+              Food sharing
+            </CustomTypography>
+          </div>
+        </h1>
+        <CustomInput placeholder="Search..." bgColor="white" width="658px" />
+      </div>
+    </div>
+  );
+};
+
+ContentHeader.propTypes = {
+  bgImage: PropTypes.string,
+};
+
+ContentHeader.defaultProps = {
+  bgImage:
+  'https://images.unsplash.com/photo-1498579809087-ef1e558fd1da?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80?auto=compress,format',
+};
+
+export default ContentHeader;
